784d18ed4d7530093d8efbfa8ce752acf0984693
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2019-09-28 17:43:00 -0400

    Python service functional

--- src/rostopic2file_node.py
+++ src/rostopic2file_node.py
@@ -5,31 +5,37 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from rostopic2file.srv import Rostopic2File, Rostopic2FileResponse
 
-def saveImage(req):
-    rostopic = req.rostopic
-    filepath = req.filepath
+class rostopic2file:
 
-    try:
-        msg = rospy.wait_for_message(rostopic, Image, 5.0)
-        sec = msg.header.stamp.sec
-        nsec = msg.header.stamp.nsec
+    def __init__(self):
+        self.bridge = CvBridge()
+        s = rospy.Service('rostopic2file', Rostopic2File, self.saveImage)
+        print "Ready to save images..."
 
-        filename = "img_" + sec + "_" + nsec + ".png"
+    def saveImage(self, req):
+        rostopic = req.rostopic
+        filepath = req.filepath
 
         try:
-            image = self.bridge.imgmsg_to_cv2(msg, msg.encoding)
-        except CvBridgeError:
-            return Rostopic2FileResponse("Writing image file failed!")
+            msg = rospy.wait_for_message(rostopic, Image, 5.0)
+            sec = msg.header.stamp.secs
+            nsec = msg.header.stamp.nsecs
 
-        cv2.imwrite(filepath + filename, image)
-        return Rostopic2FileResponse("Saved " + filename)
-    except rospy.exceptions.ROSException:
-        return Rostopic2File("No messages received in 5 seconds. Timeout!")
+            filename = "img_" + str(sec) + "_" + str(nsec) + ".png"
+
+            try:
+                image = self.bridge.imgmsg_to_cv2(msg, msg.encoding)
+            except CvBridgeError:
+                return Rostopic2FileResponse("ERROR! Writing image file failed!")
+
+            cv2.imwrite(filepath + filename, image)
+            return Rostopic2FileResponse("Saved " + filename)
+        except rospy.exceptions.ROSException:
+            return Rostopic2FileResponse("Error! No messages received in 5 seconds. Timeout!")
 
 def init():
+    rf = rostopic2file()
     rospy.init_node('rostopic2file', anonymous=True)
-    s = rospy.Service('rostopic2file', Rostopic2File, saveImage)
-    print "Ready to save images..."
     rospy.spin()
 
 if __name__ == '__main__':
