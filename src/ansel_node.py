ec6a9f80864bd62459806cc175f5497e72f58a05
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2019-09-30 13:22:49 -0400

    Rebranding

--- src/ansel_node.py
+++ src/ansel_node.py
@@ -4,14 +4,14 @@ import rospy
 import dynamic_reconfigure.client
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from rostopic2file.srv import Rostopic2File, Rostopic2FileResponse
+from Ansel.srv import Ansel, AnselResponse
 
-class rostopic2file:
+class ansel:
 
     def __init__(self):
         self.bridge = CvBridge()
         self.client = dynamic_reconfigure.client.Client("camera")
-        s = rospy.Service('rostopic2file', Rostopic2File, self.saveImage)
+        s = rospy.Service('ansel', Ansel, self.saveImage)
         print("Ready to save images...")
 
     def saveImage(self, req):
@@ -34,18 +34,18 @@ class rostopic2file:
                 try:
                     image = self.bridge.imgmsg_to_cv2(msg, msg.encoding)
                 except CvBridgeError:
-                    return Rostopic2FileResponse("ERROR! Writing image file failed!")
+                    return AnselResponse("ERROR! Writing image file failed!")
 
                 cv2.imwrite(filepath + '/' + filename, image)
                 rospy.loginfo("%s saved succesfully.", filename)
             except rospy.exceptions.ROSException:
-                return Rostopic2FileResponse("Error! No messages received in 5 seconds. Timeout!")
+                return AnselResponse("Error! No messages received in 5 seconds. Timeout!")
         
-        return Rostopic2FileResponse("Saved 10 files at" + filepath + ".")
+        return AnselResponse("Saved 10 files at" + filepath + ".")
 
 def init():
-    rf = rostopic2file()
-    rospy.init_node('rostopic2file', anonymous=True)
+    rf = ansel()
+    rospy.init_node('ansel', anonymous=True)
     rospy.spin()
 
 if __name__ == '__main__':
