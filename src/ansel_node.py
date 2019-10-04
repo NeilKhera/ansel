9c17850c89be7347e1866a6ffd72bb96eb53ea38
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2019-10-04 15:30:19 -0400

    HDR works! (timestamps still inaccurate)

--- src/ansel_node.py
+++ src/ansel_node.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import datetime
+import numpy as np
 import cv2
 import rospy
 import dynamic_reconfigure.client
+from fractions import Fraction
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from ansel.srv import Ansel, AnselResponse
@@ -16,7 +18,7 @@ class ansel:
         print("Ready to save images...")
 
     def saveImage(self, req):
-        self.client.update_configuration({'gain_auto': 'Off', 'exposure_auto': 'Continuous', 'exposure_auto_alg': 'Mean', 'exposure_auto_max': '500000'})
+        self.client.update_configuration({'gain_auto': 'Off', 'exposure_auto': 'Continuous', 'exposure_auto_alg': 'Mean', 'exposure_auto_max': '60000000'})
         rostopic = req.rostopic
         filepath = req.filepath
         rospy.loginfo("Request to save %s received. Saving at %s.", rostopic, filepath)
@@ -27,7 +29,6 @@ class ansel:
         image_arr = []
         time_arr = []
         old_grey = 0
-        time = 1.00
 
         for i in range(req.image_count):
             rospy.loginfo("Saving image %d...", i)
@@ -41,35 +42,43 @@ class ansel:
                 nsec = msg.header.stamp.nsecs
                 filename = "img_" + str(i) + "_" + str(sec) + "_" + str(nsec) + ".png"
 
-                if i != 0:
-                    time = time * pow(2, (new_grey - old_grey) / 10)
+                if i == 0:
+                    time = 1.0
+                else:
+                    base = 2.0
+                    exp = (float) (new_grey - old_grey) / 10
+                    time = time * (base ** exp)
                 time_arr.append(time)
                 old_grey = new_grey
 
                 try:
                     image = self.bridge.imgmsg_to_cv2(msg, msg.encoding)
+                    image_arr.append(image)
                 except CvBridgeError:
                     return AnselResponse("ERROR! Writing image file failed!")
 
-                image_arr.append(image)
-
                 cv2.imwrite(filepath + "/" + filename, image)
-                rospy.loginfo("%s saved succesfully.", filename)
+                rospy.loginfo("%s saved successfully.", filename)
             except rospy.exceptions.ROSException:
                 return AnselResponse("Error! No messages received in 5 seconds. Timeout!")
         
         if (req.hdr):
+            time_arr = np.asarray(time_arr, dtype=np.float32)
+
+            rospy.loginfo("Saving HDR image...")
             alignMTB = cv2.createAlignMTB()
             alignMTB.process(image_arr, image_arr)
 
             calibrateDebevec = cv2.createCalibrateDebevec()
-            responseDebevec = calibrateDebevec.process(images, times)
+            responseDebevec = calibrateDebevec.process(image_arr, time_arr)
 
             mergeDebevec = cv2.createMergeDebevec()
-            hdrDebevec = mergeDebevec.process(images, times, responseDebevec)
+            hdrDebevec = mergeDebevec.process(image_arr, time_arr, responseDebevec)
             
             DT = datetime.datetime.now()
-            cv2.imwrite(filepath + "/" + "img_" + DT.second + "_" + DT.microsecond + ".hdr", hdrDebevec)
+            cv2.imwrite(filepath + "/" + "img_" + str(DT.second) + "_" + str(DT.microsecond) + ".hdr", hdrDebevec)
+            rospy.loginfo("HDR image saved successfully.")
+
             return AnselResponse("Saved " + str(req.image_count) + " images and an HDR image at " + filepath + ".")
 
         return AnselResponse("Saved " + str(req.image_count) + " images at " + filepath + ".")
